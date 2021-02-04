073f7af678ddde37cc7b64ccd05ea811be9eb9c3
Osbroderr <70812191+Osbroderr@users.noreply.github.com>
2021-02-04 15:04:02 +0100

    up

---
+++ script.js
@@ -0,0 +1,10 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Bonjour', '&amp; a second sentence.'],
+  typeSpeed: 40,
+  loop: true
+};
+
+var typed = new Typed('.element', options);

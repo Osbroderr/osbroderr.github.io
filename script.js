ead3340ba1254b18c72e903a1c7fff88d5ead24d
Osbroderr <70812191+Osbroderr@users.noreply.github.com>
2021-02-11 11:11:45 +0100

    up

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var options = {
   strings: ['Hello','Hi','Hola','Hallo','Haai','Ola','Kaixo','Moni','Aloha','Bonjour','Ciao','Hej'],
   typeSpeed: 100,
-  loop: true
+  loop: true,
+  fadeOut: true,
+  fadeOutDelay: 700,
 };
 
 var typed = new Typed('.element', options);
